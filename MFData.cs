7175734d13a923663ae28d9dd520d2270a7042e5
HyperKwonHyukmin <khm7529@gmail.com>
2026-02-03 10:33:12 +0900

    Update MFData.cs

--- MFData.cs
+++ MFData.cs
@@ -12,20 +12,20 @@ namespace MooringFitting2026.RawData
     public string Type;
     public double[] Location = new double[3];
     public List<(double, double, double)> RigidRange = new List<(double, double, double)> { };
-    public double Mass;
+    public double SWL;
     public double a;
     public double b;
     public double c;
     public double tow;
 
     public MFData(string mfID, string mfType, double[] mfLocation, 
-      List<(double, double, double)> rigidRange, double mass, double a, double b, double c, double tow)
+      List<(double, double, double)> rigidRange, double swl, double a, double b, double c, double tow)
     {
       this.ID = mfID;
       this.Type = mfType;
       this.Location = mfLocation;
       RigidRange = rigidRange;
-      Mass = mass;
+      SWL = swl;
       this.a = a;
       this.b = b;
       this.c = c;
