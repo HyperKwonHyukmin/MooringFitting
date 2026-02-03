2bf17bab86e410c3d9bca42c3e77a2b54a260090
HyperKwonHyukmin <khm7529@gmail.com>
2026-02-03 09:45:10 +0900

    Update BdfExporter.cs

--- BdfExporter.cs
+++ BdfExporter.cs
@@ -1,20 +1,27 @@
 using MooringFitting2026.Model.Entities;
 using MooringFitting2026.Exporters;
+using MooringFitting2026.Modifier.ElementModifier; // RigidInfo 사용을 위해 추가
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO; // Path, File 사용을 위해
 
 namespace MooringFitting2026.Exporters
 {
   public static class BdfExporter
   {
-    // [수정] spcList 인자 추가 (기본값 null)
-    public static void Export(FeModelContext context, string CsvPath, string stageName, List<int> spcList = null)
+    // [수정] rigidMap 인자 추가 (기본값 null)
+    public static void Export(
+        FeModelContext context,
+        string CsvPath,
+        string stageName,
+        List<int> spcList = null,
+        Dictionary<int, MooringFittingConnectionModifier.RigidInfo> rigidMap = null) // <--- 추가됨
     {
-      // [수정] BdfBuilder 생성자에 spcList 전달
-      var bdfBuilder = new BdfBuilder(101, context, spcList);
+      // [수정] BdfBuilder 생성자에 rigidMap 전달
+      var bdfBuilder = new BdfBuilder(101, context, spcList, rigidMap);
 
       bdfBuilder.Run();
       string newBdfName = stageName + ".bdf";
