0c60e6fe6de884bb181702a8d1d8694ac26216c6
HyperKwonHyukmin <khm7529@gmail.com>
2026-02-05 11:14:51 +0900

    Update BdfExporter.cs

--- BdfExporter.cs
+++ BdfExporter.cs
@@ -1,6 +1,10 @@
 using MooringFitting2026.Exporters;
 using MooringFitting2026.Model.Entities;
 using MooringFitting2026.Modifier.ElementModifier;
+using MooringFitting2026.Services.Load;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 public static class BdfExporter
 {
@@ -10,10 +14,17 @@ public static class BdfExporter
       string stageName,
       List<int> spcList = null,
       Dictionary<int, MooringFittingConnectionModifier.RigidInfo> rigidMap = null,
-      List<ForceLoad> forceLoads = null) // [추가] 인자
+      List<ForceLoad> forceLoads = null)
   {
-    // [수정] forceLoads 전달
-    var bdfBuilder = new BdfBuilder(101, context, spcList, rigidMap, forceLoads);
+    // [수정] 하중 리스트에서 최대 Load Case ID 계산
+    int maxLoadCaseID = 1;
+    if (forceLoads != null && forceLoads.Count > 0)
+    {
+      maxLoadCaseID = forceLoads.Max(f => f.LoadCaseID);
+    }
+
+    // [수정] 계산된 maxLoadCaseID를 생성자에 전달
+    var bdfBuilder = new BdfBuilder(101, context, spcList, rigidMap, forceLoads, maxLoadCaseID);
 
     bdfBuilder.Run();
     string newBdfName = stageName + ".bdf";
